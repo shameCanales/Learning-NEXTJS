69a6ed2e060415308fa753d1b0e96e5dbd6951bb
shameCanales <canalesshame@gmail.com>
2025-08-03 20:47:51 +0800

    learned part 429 to 430

--- app/layout.js
+++ app/layout.js
@@ -8,9 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children} <p>hello</p>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,4 +1,8 @@
+// this component is rendered on the server side. if you have a console.log here in this component then it won't show in the browser console
+
 export default function Home() {
+  console.log("this is rendered on the server");
+
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
@@ -7,3 +11,12 @@ export default function Home() {
     </main>
   );
 }
+
+// ROUTING : File based routing
+// Nextjs works with react server components
+// -Components which require a special "environment" NEXTJS provides such an environment
+// React Server Components (RSC) - Rendered only on the server never on the client
+
+//adding another route via the file system
+//localhost:3000/about - for example we want to have an about page
+//define routes using folders
