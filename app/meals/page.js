c0e10b1c672a4dd705a0400b33ad9e63c510df0d
shameCanales <canalesshame@gmail.com>
2025-08-04 21:14:07 +0800

    completed part 437 to 438

---
+++ app/meals/page.js
@@ -0,0 +1,3 @@
+export default function MealsPage() {
+  return <h1>This is Meals Page</h1>;
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
+      <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
       </h1>
+      <ul>
+        <li>
+          <Link href="/meals">Meals</Link>
+        </li>
+        <li>
+          <Link href="/meals/share">Share Meal</Link>
+        </li>
+        <li>
+          <Link href="/community">Community</Link>
+        </li>
+      </ul>
     </main>
   );
 }
