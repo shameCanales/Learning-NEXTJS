baff910fb590d680903bd9153f370685d7c7f70c
shameCanales <canalesshame@gmail.com>
2025-08-05 22:58:12 +0800

    completed part 444 to 447

--- app/components/main-header/main-header.js
+++ app/components/main-header/main-header.js
@@ -3,8 +3,10 @@ import classes from "./main-header.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
+  // console.log("Executing na...");
   return (
     <>
       <MainHeaderBackground />
@@ -17,10 +19,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/meals">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,4 @@
 export default function MealsPage() {
+  console.log('meals page')
   return <h1>This is Meals Page</h1>;
 }
