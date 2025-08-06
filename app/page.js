55ecd61fd1b7afc1f867b5b7c0901ee19a905de9
shameCanales <canalesshame@gmail.com>
2025-08-06 21:21:49 +0800

    completed part448 to 455

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
-
 import Link from "next/link";
 import classes from "./page.module.css";
-import ImageSlideshow from "./components/images/image-slideshow";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
@@ -52,4 +51,3 @@ export default function Home() {
     </>
   );
 }
-
