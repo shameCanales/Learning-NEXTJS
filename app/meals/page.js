cde1f62b19884cbcfe6e7f116592a67ef0005974
shameCanales <canalesshame@gmail.com>
2025-08-06 21:26:33 +0800

    hide meal.db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,4 +1,35 @@
+import classes from "./page.module.css";
+import Link from "next/link";
+import { getMeals } from "@/lib/meals";
+import MealsGrid from "@/components/meals/meals-grid";
+import { Suspense } from "react";
+import MealsLoadingPage from "./loading-out";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
 export default function MealsPage() {
-  console.log('meals page')
-  return <h1>This is Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
