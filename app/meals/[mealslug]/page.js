83e4f096c2a9c402fa838fda77728afb54cb96bd
shameCanales <canalesshame@gmail.com>
2025-08-07 22:21:20 +0800

    completed part456 to 466

--- app/meals/[mealslug]/page.js
+++ app/meals/[mealslug]/page.js
@@ -1,9 +1,14 @@
 import classes from "./page.module.css";
 import Image from "next/image";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound(); //provided by nextjs. will show the closest error page when a meal is not found
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
